99a4e84b967ee17c8da81c570fc9da81310096de
Jacob Chvatal <jake@isnt.online>
2021-03-26 19:16:10 -0700

    finish

--- exercises/054_manypointers.zig
+++ exercises/054_manypointers.zig
@@ -32,7 +32,10 @@ pub fn main() void {
     // we can CONVERT IT TO A SLICE. (Hint: we do know the length!)
     //
     // Please fix this line so the print below statement can print it:
-    const zen12_string: []const u8 = zen_manyptr;
+
+    // Convert a many item pointer to a slice by selecting the entire array pointed to as a slice.
+    // It's implicitly dereferenced when taking the slice.
+    const zen12_string: []const u8 = zen_manyptr[0..21];
 
     // Here's the moment of truth!
     std.debug.print("{s}\n", .{zen12_string});
